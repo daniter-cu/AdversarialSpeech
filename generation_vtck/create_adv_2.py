bcb9e17943c854688f4f6f7de637a220fa66bac1
Dan Iter <daniter@stanford.edu>
2017-06-05 21:14:47 -0700

    added noise object

--- generation_vtck/create_adv_2.py
+++ generation_vtck/create_adv_2.py
@@ -20,9 +20,16 @@ tf.sg_verbosity(10)
 
 batch_size = 1     # batch size
 
+index = 0
+
 #
 # inputs
 #
+corpus = SpeechCorpus(batch_size=batch_size * tf.sg_gpus())
+mfccs = []
+for mfcc_file in corpus.mfcc_file:
+  mfcc = np.load(mfcc_file, allow_pickle=False)
+  mfccs.append(mfcc.reshape((1, mfcc.shape[0], mfcc.shape[1])).transpose([0,2,1]))
 
 # vocabulary size
 voca_size = data.voca_size
@@ -30,11 +37,15 @@ voca_size = data.voca_size
 # mfcc feature of audio
 x = tf.placeholder(dtype=tf.sg_floatx, shape=(batch_size, None, 20))
 
+#noise = tf.get_variable("noise", shape=(batch_size, mfccs[index].shape[1], 20), initializer=tf.zeros_initializer())
+
+perturbed_input = x #+ noise
+
 # sequence length except zero-padding
 seq_len = tf.not_equal(x.sg_sum(axis=2), 0.).sg_int().sg_sum(axis=1)
 
 # encode audio feature
-logit = get_logit(x, voca_size=voca_size)
+logit = get_logit(perturbed_input, voca_size=voca_size)
 
 # ctc decoding
 decoded, _ = tf.nn.ctc_beam_search_decoder(logit.sg_transpose(perm=[1, 0, 2]), seq_len, merge_repeated=False)
@@ -42,11 +53,7 @@ decoded, _ = tf.nn.ctc_beam_search_decoder(logit.sg_transpose(perm=[1, 0, 2]), s
 # to dense tensor
 y = tf.sparse_to_dense(decoded[0].indices, decoded[0].dense_shape, decoded[0].values) + 1
 
-data = SpeechCorpus(batch_size=batch_size * tf.sg_gpus())
-mfccs = []
-for mfcc_file in data.mfcc_file:
-  mfcc = np.load(mfcc_file, allow_pickle=False)
-  mfccs.append(mfcc.reshape((1, mfcc.shape[0], mfcc.shape[1])).transpose([0,2,1]))
+
 
 
 # run network
@@ -55,12 +62,19 @@ with tf.Session() as sess:
     # init variables
     tf.sg_init(sess)
 
+    
+
+    all_vars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES) + \
+          tf.get_collection(tf.GraphKeys.SAVEABLE_OBJECTS)
+
+    vars_to_train = [el for el in all_vars if 'noise' not in el.name]
+
     # restore parameters
-    saver = tf.train.Saver()
+    saver = tf.train.Saver(vars_to_train)
+    #saver = tf.train.Saver()
     saver.restore(sess, tf.train.latest_checkpoint('asset/train'))
     # run session
-    for i in xrange(5):
-      label = sess.run(y, feed_dict={x: mfccs[i]})
+    label = sess.run(y, feed_dict={x: mfccs[index]})
 
-      # print label
-      print_index(label)
+    # print label
+    print_index(label)
