2e777aec6908cf326e1ca1d5e0ef61a6b1be7977
Mike Jermann <mjermann@gmail.com>
2017-06-06 18:41:39 -0700

    Updated InverseMfcc to support adding noise

--- InverseMfcc.py
+++ InverseMfcc.py
@@ -25,6 +25,9 @@ class InverseMfcc(object):
         self.hop_length=512
         self.num_mfcc=20
         self.include_deltas=False
+        self.noise_scale = 0.0
+        self.mfcc_noise = 0.0
+
         #### Don't change these for now ####
         self.n_mel_recon = 128 
         self.n_fft_recon = 2048
@@ -35,7 +38,9 @@ class InverseMfcc(object):
                           num_mfcc=None,
                           include_deltas=None,
                           n_mel_recon=None,
-                          n_fft_recon=None):
+                          n_fft_recon=None,
+                          noise_scale=None,
+                          mfcc_noise=None):
         '''Modify any parameters passed in'''
     
         if (sr != None):
@@ -50,6 +55,10 @@ class InverseMfcc(object):
             self.n_mel_recon = n_mel_recon
         if (n_fft_recon != None):
             self.n_fft_recon = n_fft_recon
+        if (noise_scale != None):
+            self.noise_scale = noise_scale
+        if (mfcc_noise != None):
+            self.mfcc_noise = mfcc_noise
 
     def transform(self, mfcc=None):
         #### Can overwrite given MFCC ####
@@ -75,13 +84,20 @@ class InverseMfcc(object):
         dctm = librosa.filters.dct(n_mfcc, self.n_mel_recon)
         mel_basis = librosa.filters.mel(self.sr, self.n_fft_recon)
 
+        # Optional - Add additional noise
+        noise = self.mfcc_noise*np.random.randn(self.mfcc.shape[0], self.mfcc.shape[1])
+        self.mfcc += noise
+
         #TODO: Figure out if should modify this
-        bin_scaling = 1.0 / np.maximum(0.1, np.sum(np.dot(mel_basis.T, mel_basis), axis=0))
+        bin_scaling = 1.0 / np.maximum(0.01, np.sum(np.dot(mel_basis.T, mel_basis), axis=0))
 
         #Approx squared magnitude
         recon_stft = bin_scaling[:, np.newaxis] * np.dot(mel_basis.T,
                                                          self._invlogamplitude(np.dot(dctm.T, mfccs)))
-
+        # Optional - Add noise to reconstruction
+        noise = self.noise_scale*np.ones(recon_stft.shape)
+        recon_stft += noise
+        
         # Impose reconstructed magnitude on white noise STFT
         #excitation = np.random.randn(self.orig.shape[0])
         excitation = np.random.randn(self.hop_length*(mfccs.shape[1]-1))
@@ -102,7 +118,8 @@ class InverseMfcc(object):
             print "No MFCC constructed"
             return
         return self.mfcc
-
+         
     def _invlogamplitude(self, S):
         '''librosa.logamplitude is actually 10*log10, so invert that.'''
         return 10.0 ** (S / 10.0)
+
